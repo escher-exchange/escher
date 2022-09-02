19cf9d1109792c9bed904f97695ab5fba9ac7687
Angelo Gregorio Lovatto <12701614+0xangelo@users.noreply.github.com>
2022-09-02 14:47:16 +0000

    test(CH): settle 2 longs in a market

--- clearing-house/src/tests/integration/tests.rs
+++ clearing-house/src/tests/integration/tests.rs
@@ -1253,3 +1253,73 @@ fn should_handle_one_long() {
         assert!(get_position(&ALICE, &market_id).is_none());
     })
 }
+
+#[test]
+fn should_handle_both_longs() {
+    let (alice_col0, bob_col0) = (UNIT * 100, UNIT * 100);
+    ExtBuilder {
+        native_balances: vec![(ALICE, UNIT), (BOB, UNIT)],
+        balances: vec![(ALICE, USDC, alice_col0), (BOB, USDC, bob_col0)],
+        ..Default::default()
+    }
+    .build()
+    .execute_with(|| {
+        let asset_id = DOT;
+        set_oracle_for(asset_id, 1_000);
+
+        assert_ok!(TestPallet::create_market(
+            Origin::signed(ALICE),
+            MarketConfig::default()
+        ));
+
+        assert_ok!(TestPallet::deposit_collateral(
+            Origin::signed(ALICE),
+            USDC,
+            alice_col0
+        ));
+        assert_ok!(TestPallet::deposit_collateral(
+            Origin::signed(BOB),
+            USDC,
+            bob_col0
+        ));
+
+        let market_id = Zero::zero();
+        assert_ok!(TestPallet::open_position(
+            Origin::signed(ALICE),
+            market_id,
+            Long,
+            alice_col0,
+            0
+        ));
+        assert_ok!(TestPallet::open_position(
+            Origin::signed(BOB),
+            market_id,
+            Long,
+            bob_col0,
+            0
+        ));
+
+        let now = get_time_now();
+        assert_ok!(TestPallet::close_market(
+            Origin::root(),
+            market_id,
+            now + 12
+        ));
+
+        advance_blocks_by(1, 12);
+
+        assert_ok!(TestPallet::settle_position(
+            Origin::signed(ALICE),
+            market_id
+        ));
+        assert_ok!(TestPallet::settle_position(
+            Origin::signed(BOB),
+            market_id
+        ));
+
+        let (alice_col, bob_col) = (get_collateral(&ALICE), get_collateral(&BOB));
+        assert!(alice_col > alice_col0);
+        assert!(bob_col < bob_col0);
+        assert_eq!(alice_col + bob_col, alice_col0 + bob_col0);
+    })
+}
